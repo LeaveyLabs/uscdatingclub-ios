44836fc325da601a00c7dbdc36cd9b51434d7f63
kevinsun127 <kevinsun@usc.edu>
2023-01-12 12:43:40 -0800

    questions

---
+++ uscdatingclub-ios/API/QuestionAPI.swift
@@ -0,0 +1,73 @@
+//
+//  QuestionAPI.swift
+//  uscdatingclub-ios
+//
+//  Created by Kevin Sun on 1/12/23.
+//
+
+import Foundation
+
+
+//https://github.com/kean/Nuke
+
+struct QuestionError: Codable {
+    let question_id: [String]?
+    let answer: [String]?
+    
+    let non_field_errors: [String]?
+    let detail: String?
+}
+
+struct Question: Codable {
+    let id: Int
+    let category: String
+    let isNumerical: Bool
+    let isMultipleAnswer: Bool
+}
+
+class QuestionAPI {
+    // Paths to API endpoints
+    enum Endpoints: String {
+        case getQuestions = "get-questions/"
+    }
+    
+    // Parameters for API
+    enum ParameterKeys: String {
+        case questionId = "question_id"
+        case answer = "answer"
+    }
+    
+    static let QUESTION_RECOVERY_MESSAGE = "try again later"
+    
+    static func throwAPIError(error: QuestionError) throws {
+        if let questionErrors = error.question_id,
+            let questionError = questionErrors.first {
+            throw APIError.ClientError(questionError, QUESTION_RECOVERY_MESSAGE)
+        }
+        if let answerErrors = error.answer,
+            let answerError = answerErrors.first {
+            throw APIError.ClientError(answerError, QUESTION_RECOVERY_MESSAGE)
+        }
+    }
+    
+    static func filterQuestionErrors(data:Data, response:HTTPURLResponse) throws {
+        let statusCode = response.statusCode
+        
+        if isSuccess(statusCode: statusCode) { return }
+        if isClientError(statusCode: statusCode) {
+            let error = try JSONDecoder().decode(QuestionError.self, from: data)
+            try throwAPIError(error: error)
+        }
+        throw APIError.Unknown
+    }
+    
+    static func getQuestions() async throws -> [Question] {
+        let url = "\(Env.BASE_URL)\(Endpoints.getQuestions.rawValue)"
+        let (data, response) = try await BasicAPI.basicHTTPCallWithToken(url: url, jsonData: Data(), method: HTTPMethods.PATCH.rawValue)
+        try filterQuestionErrors(data: data, response: response)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode([Question].self, from: data)
+    }
+}
+
